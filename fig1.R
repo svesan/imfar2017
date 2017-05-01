9bdb4ac17f54a9fc023dffce9711400d49d04794
svesan <sven.sandin@ki.se>
2017-05-01 10:32:33 -0400

    commit to work

--- fig1.R
+++ fig1.R
@@ -21,19 +21,79 @@ xc=c("PD", "Mood", "Anxiety", "Personal", "ID", "ASD", "Cognition", "Hernia", "D
 estim=data.frame(x,pe,lc,uc,xc)
 
 
+p2 <- ggplot(estim, aes(pe, xc, size=10)) + theme_bw(base_size=10)
+
 #-- Set the order of the labels
 #-- http://www.cookbook-r.com/Graphs/Axes_(ggplot2)/
-p2 + geom_errorbar(aes(x = xc, ymin = lc, ymax = uc, size=1), width = 0.001, color="blue") + scale_y_log10(limits=c(0.25, 15), breaks=c(0.25, 1, 5, 15))+ coord_flip()+ylab("RR")+xlab("Disorder")+scale_x_discrete(limits=c("PD", "Mood", "Anxiety", "Personal", "ID", "ASD", "Cognition", "Hernia", "Diab"))
+p2 + geom_errorbar(aes(pe, x = xc, ymin = lc, ymax = uc, size=1), width = 0.001, color="#56B4E9") + scale_y_log10(limits=c(0.25, 15), breaks=c(0.25, 1, 5, 15))+ coord_flip()+ylab("RR")+xlab("Disorder")+scale_x_discrete(limits=c("PD", "Mood", "Anxiety", "Personal", "ID", "ASD", "Cognition", "Hernia", "Diab"))
 
 #values=c("#999999", "#E69F00", "#56B4E9")
 
 #-- Add reference line
 #-- Font size axis text
 #-- Remove yaxis label
-p2 + geom_errorbar(aes(x = xc, ymin = lc, ymax = uc, size=1), width = 0.001, color="#56B4E9") + scale_y_log10(limits=c(0.5, 15), breaks=c(0.5, 1, 2, 5, 10, 15))+ylab("Relative Risk")+xlab("Disorder")+scale_x_discrete(limits=c("ASD", "ID", "PD", "Mood", "Anxiety", "Personal", "Cognition", "Hernia", "Diab"))+ geom_hline(yintercept = 1)+theme(axis.text.x = element_text(face="plain", color="#993333", size=14, angle=0), axis.title.y=element_blank(), axis.text.y = element_text(face="plain", color="#993333", size=14, angle=0))+ coord_flip()
+p2 + geom_errorbar(aes(x = xc, ymin = lc, ymax = uc, size=1), width = 0.001, color="#56B4E9") + scale_y_log10(limits=c(0.5, 15), breaks=c(0.5, 0.8, 1, 1.25, 2, 3, 5, 7, 10, 15))+ylab("Relative Risk")+xlab("Disorder")+scale_x_discrete(limits=c("ASD", "ID", "PD", "Mood", "Anxiety", "Personal", "Cognition", "Hernia", "Diab"))+ geom_hline(yintercept = 1)+theme(axis.text.x = element_text(face="plain", color="#993333", size=14, angle=0), axis.title.y=element_blank(), axis.text.y = element_text(face="plain", color="#993333", size=14, angle=0))+ coord_flip()
 
 last_plot() + theme(axis.title.x=element_text(face="bold", color="#993333", size=14))
 
 #-- remove legend
 last_plot() + theme(legend.position="none")
+
+last_plot() + theme(axis.ticks.y = element_blank())
+p0=last_plot()
+
+#-- Add sweden RRR ASD
+slc=rep(NA,9);suc=rep(NA,9);
+slc[6]=8; suc[6]=12
+
+p0+geom_errorbar(aes(x = xc[6], ymin = slc[6], ymax = suc[6], size=0.9), width = 0.01, color="black")
+
+#--- Alternatively, a complete new figure with Sweden added
+
+spe=c(pe,10.3); slc=c(lc,9.4); suc=c(uc,11.3); sxc=c(xc, "ASD Swe")
+sestim=data.frame(spe, slc, suc, sxc)
+
+swecol = c(rep("#56B4E9", 9), "#E69F00")
+p3 <- ggplot(sestim, aes(sxc, size=10, color=swecol)) + theme_bw(base_size=10)
+
+p3 + geom_errorbar(aes(
+  x = sxc, ymin = slc, ymax = suc, size = 1
+), width = 0.001) + scale_y_log10(limits = c(0.5, 15),
+                                  breaks = c(0.5, 0.8, 1, 1.25, 2, 3, 5, 7, 10, 15)) + ylab("Relative Risk") +
+  xlab("Disorder") + scale_x_discrete(
+    limits = c(
+      "ASD Swe",
+      "ASD",
+      "ID",
+      "PD",
+      "Mood",
+      "Anxiety",
+      "Personal",
+      "Cognition",
+      "Hernia",
+      "Diab"
+    )
+  ) + geom_hline(yintercept = 1) + theme(
+    axis.text.x = element_text(
+      face = "plain",
+      color = "#993333",
+      size = 14,
+      angle = 0
+    ),
+    axis.title.y = element_blank(),
+    axis.text.y = element_text(
+      face = "plain",
+      color = "#993333",
+      size = 14,
+      angle = 0
+    )
+  ) + coord_flip()
+
+last_plot()+ theme(axis.title.x=element_text(face="bold", color="#993333", size=14))
+
+#-- remove legend
+last_plot() + theme(legend.position="none")
+
+last_plot() + theme(axis.ticks.y = element_blank())
+
 
